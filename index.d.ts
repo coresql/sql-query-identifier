c7fc8c11256f3b384d5be6c051303769f7a2264d
Matthew Peveler <matt.peveler@gmail.com>
2020-12-31 18:10:50 -0500

    add typing to package
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+export type Dialect = 'generic' | 'mysql' | 'psql' | 'sqlite' | 'mssql';
+
+export type StatementType =
+  'SELECT'
+  | 'INSERT'
+  | 'UPDATE'
+  | 'DELETE'
+  | 'CREATE_TABLE'
+  | 'CREATE_DATABASE'
+  | 'CREATE_TRIGGER'
+  | 'DROP_TABLE'
+  | 'DROP_DATABASE'
+  | 'TRUNCATE'
+  | 'UNKNOWN';
+
+export type ExecutionType = 'LISTING' | 'MODIFICATION' | 'INFORMATION' | 'UNKNOWN';
+
+export interface Options {
+  /**
+   * Disables strict mode which will ignore unknown types (defaults to true).
+   */
+  strict?: boolean;
+  /**
+   * Set dialect for database specific parsing (defaults to generic)
+   */
+  dialect?: Dialect;
+}
+
+export interface Result {
+  start: number;
+  end: number;
+  text: string;
+  type: StatementType;
+  executionType: ExecutionType;
+}
+
+export function identify(query: string, options?: Options): Result[];
