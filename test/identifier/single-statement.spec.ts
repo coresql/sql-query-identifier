324bfb9d1b17f13a7f75fe60c93961d628714379
Matthew Peveler <matt.peveler@gmail.com>
2021-07-23 13:07:26 +0000

    group trigger and function tests together
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- test/identifier/single-statement.spec.ts
+++ test/identifier/single-statement.spec.ts
@@ -222,278 +222,268 @@ describe('identifier', function () {
       })
     });
 
-    it('should identify sqlite "CREATE TRIGGER" statement', function () {
-      const actual = identify('CREATE TRIGGER sqlmods AFTER UPDATE ON bar FOR EACH ROW WHEN old.yay IS NULL BEGIN UPDATE bar SET yay = 1 WHERE rowid = NEW.rowid; END;', { dialect: 'sqlite' });
-      const expected = [
-        {
-          start: 0,
-          end: 134,
-          text: 'CREATE TRIGGER sqlmods AFTER UPDATE ON bar FOR EACH ROW WHEN old.yay IS NULL BEGIN UPDATE bar SET yay = 1 WHERE rowid = NEW.rowid; END;',
-          type: 'CREATE_TRIGGER',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
+    describe('identify "CREATE TRIGGER" statements', () => {
+      it('should identify sqlite "CREATE TRIGGER" statement', function () {
+        const actual = identify('CREATE TRIGGER sqlmods AFTER UPDATE ON bar FOR EACH ROW WHEN old.yay IS NULL BEGIN UPDATE bar SET yay = 1 WHERE rowid = NEW.rowid; END;', { dialect: 'sqlite' });
+        const expected = [
+          {
+            start: 0,
+            end: 134,
+            text: 'CREATE TRIGGER sqlmods AFTER UPDATE ON bar FOR EACH ROW WHEN old.yay IS NULL BEGIN UPDATE bar SET yay = 1 WHERE rowid = NEW.rowid; END;',
+            type: 'CREATE_TRIGGER',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
 
-    it('should identify sqlite "CREATE TRIGGER" statement with case', function () {
-      const sql = `CREATE TRIGGER DeleteProduct
-      BEFORE DELETE ON Product
-      BEGIN
-          SELECT CASE WHEN (SELECT Inventory.InventoryID FROM Inventory WHERE Inventory.ProductID = OLD.ProductID and Inventory.Quantity=0) IS NULL
-          THEN RAISE(ABORT,'Error code 82')
-          END;
-          -- If RAISE was called, next isntructions are not executed.
-          DELETE from inventory where inventory.ProductID=OLD.ProductID;
-      END;`;
-
-      const actual = identify(sql);
-      const expected = [
-        {
-          start: 0,
-          end: 431,
-          text: sql,
-          type: 'CREATE_TRIGGER',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
-
-    it('should identify SQLSERVER "CREATE TRIGGER" statement', function () {
-      const query = `CREATE TRIGGER Purchasing.LowCredit ON Purchasing.PurchaseOrderHeader
-        AFTER INSERT
-        AS
-        IF (ROWCOUNT_BIG() = 0)
-        RETURN;
-        IF EXISTS (SELECT *
-                  FROM Purchasing.PurchaseOrderHeader AS p
-                  JOIN inserted AS i
-                  ON p.PurchaseOrderID = i.PurchaseOrderID
-                  JOIN Purchasing.Vendor AS v
-                  ON v.BusinessEntityID = p.VendorID
-                  WHERE v.CreditRating = 5
-                  )
+      it('should identify sqlite "CREATE TRIGGER" statement with case', function () {
+        const sql = `CREATE TRIGGER DeleteProduct
+        BEFORE DELETE ON Product
         BEGIN
-        RAISERROR ('A vendor''s credit rating is too low to accept new
-        purchase orders.', 16, 1);
-        ROLLBACK TRANSACTION;
-        RETURN
+            SELECT CASE WHEN (SELECT Inventory.InventoryID FROM Inventory WHERE Inventory.ProductID = OLD.ProductID and Inventory.Quantity=0) IS NULL
+            THEN RAISE(ABORT,'Error code 82')
+            END;
+            -- If RAISE was called, next isntructions are not executed.
+            DELETE from inventory where inventory.ProductID=OLD.ProductID;
         END;`;
-      const actual = identify(query, { dialect: 'mssql' });
-      const expected = [
-        {
-          start: 0,
-          end: 671,
-          text: query,
-          type: 'CREATE_TRIGGER',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
+
+        const actual = identify(sql);
+        const expected = [
+          {
+            start: 0,
+            end: 447,
+            text: sql,
+            type: 'CREATE_TRIGGER',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
+
+      it('should identify SQLSERVER "CREATE TRIGGER" statement', function () {
+        const query = `CREATE TRIGGER Purchasing.LowCredit ON Purchasing.PurchaseOrderHeader
+          AFTER INSERT
+          AS
+          IF (ROWCOUNT_BIG() = 0)
+          RETURN;
+          IF EXISTS (SELECT *
+                    FROM Purchasing.PurchaseOrderHeader AS p
+                    JOIN inserted AS i
+                    ON p.PurchaseOrderID = i.PurchaseOrderID
+                    JOIN Purchasing.Vendor AS v
+                    ON v.BusinessEntityID = p.VendorID
+                    WHERE v.CreditRating = 5
+                    )
+          BEGIN
+          RAISERROR ('A vendor''s credit rating is too low to accept new
+          purchase orders.', 16, 1);
+          ROLLBACK TRANSACTION;
+          RETURN
+          END;`;
+        const actual = identify(query, { dialect: 'mssql' });
+        const expected = [
+          {
+            start: 0,
+            end: 707,
+            text: query,
+            type: 'CREATE_TRIGGER',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
+
+      it('should identify postgres "CREATE TRIGGER" statement', function () {
+        const actual = identify('CREATE TRIGGER view_insert INSTEAD OF INSERT ON my_view FOR EACH ROW EXECUTE PROCEDURE view_insert_row();');
+        const expected = [
+          {
+            start: 0,
+            end: 104,
+            text: 'CREATE TRIGGER view_insert INSTEAD OF INSERT ON my_view FOR EACH ROW EXECUTE PROCEDURE view_insert_row();',
+            type: 'CREATE_TRIGGER',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
     });
 
-    it('should identify postgres "CREATE TRIGGER" statement', function () {
-      const actual = identify('CREATE TRIGGER view_insert INSTEAD OF INSERT ON my_view FOR EACH ROW EXECUTE PROCEDURE view_insert_row();');
-      const expected = [
-        {
-          start: 0,
-          end: 104,
-          text: 'CREATE TRIGGER view_insert INSTEAD OF INSERT ON my_view FOR EACH ROW EXECUTE PROCEDURE view_insert_row();',
-          type: 'CREATE_TRIGGER',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
+    describe('identify "CREATE FUNCTION" statements', () => {
 
-    it('should identify postgres "CREATE FUNCTION" statement with LANGUAGE at end', function () {
-      const sql = `CREATE FUNCTION quarterly_summary_func(start_date date DEFAULT CURRENT_TIMESTAMP)
-      RETURNS TABLE (staff_name text, staff_bonus int, quarter tsrange)
-      As $$
-      DECLARE
-        employee RECORD;
-        total_bonus int;
-        sales_total int;
-        end_date date := start_date + interval '3 months';
-      BEGIN
-        FOR employee IN SELECT staff_id FROM staff LOOP
-          EXECUTE 'SELECT sum(staff_bonus), sum(sales_price) FROM sales WHERE staff_id = $1
-          AND created_at >= $2 AND created_at < $3'
-             INTO total_bonus, sales_total
-             USING employee.staff_id, start_date, end_date;
-          RAISE NOTICE 'total bonus is % and total sales is %', total_bonus, sales_total;
-         EXECUTE 'INSERT INTO sales_summary (staff_id, bonus, total_sales, period) VALUES
-                    ($1, $2, $3, tsrange($4, $5))'
-             USING employee.staff_id, total_bonus, sales_total, start_date, end_date;
+      it('should identify postgres "CREATE FUNCTION" statement with LANGUAGE at end', function () {
+        const sql = `CREATE FUNCTION quarterly_summary_func(start_date date DEFAULT CURRENT_TIMESTAMP)
+        RETURNS TABLE (staff_name text, staff_bonus int, quarter tsrange)
+        As $$
+        DECLARE
+          employee RECORD;
+          total_bonus int;
+          sales_total int;
+          end_date date := start_date + interval '3 months';
+        BEGIN
+          FOR employee IN SELECT staff_id FROM staff LOOP
+            EXECUTE 'SELECT sum(staff_bonus), sum(sales_price) FROM sales WHERE staff_id = $1
+            AND created_at >= $2 AND created_at < $3'
+              INTO total_bonus, sales_total
+              USING employee.staff_id, start_date, end_date;
+            RAISE NOTICE 'total bonus is % and total sales is %', total_bonus, sales_total;
+          EXECUTE 'INSERT INTO sales_summary (staff_id, bonus, total_sales, period) VALUES
+                      ($1, $2, $3, tsrange($4, $5))'
+              USING employee.staff_id, total_bonus, sales_total, start_date, end_date;
+          END LOOP;
+          DELETE FROM sales WHERE created_at >= start_date
+                AND created_at < end_date;
+          RETURN QUERY SELECT name, bonus, period FROM sales_summary
+                      LEFT JOIN staff on sales_summary.staff_id = staff.staff_id;
+        RETURN;
+        END;
+        $$
+        LANGUAGE plpgsql;`;
+        const actual = identify(sql, { dialect: 'psql' });
+        const expected = [
+          {
+            start: 0,
+            end: 1313,
+            text: sql,
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
+
+      it('should identify postgres "CREATE FUNCTION" statement with LANGUAGE at beginning', function () {
+        const sql = `CREATE OR REPLACE FUNCTION f_grp_prod(text)
+          RETURNS TABLE (
+            name text
+          , result1 double precision
+          , result2 double precision)
+        LANGUAGE plpgsql STABLE
+        AS
+        $BODY$
+        DECLARE
+            r      mytable%ROWTYPE;
+            _round integer;
+        BEGIN
+            -- init vars
+            name    := $1;
+            result2 := 1;       -- abuse result2 as temp var for convenience
+
+        FOR r IN
+            SELECT *
+            FROM   mytable m
+            WHERE  m.name = name
+            ORDER  BY m.round
+        LOOP
+            IF r.round <> _round THEN   -- save result1 before 2nd round
+                result1 := result2;
+                result2 := 1;
+            END IF;
+
+            result2 := result2 * (1 - r.val/100);
+            _round  := r.round;
         END LOOP;
-        DELETE FROM sales WHERE created_at >= start_date
-               AND created_at < end_date;
-        RETURN QUERY SELECT name, bonus, period FROM sales_summary
-                     LEFT JOIN staff on sales_summary.staff_id = staff.staff_id;
-       RETURN;
-      END;
-      $$
-      LANGUAGE plpgsql;`;
-      const actual = identify(sql, { dialect: 'psql' });
-      const expected = [
-        {
-          start: 0,
-          end: 1268,
-          text: sql,
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
 
-    it('should identify postgres "CREATE FUNCTION" statement with LANGUAGE at beginning', function () {
-      const sql = `CREATE OR REPLACE FUNCTION f_grp_prod(text)
-      RETURNS TABLE (
-        name text
-      , result1 double precision
-      , result2 double precision)
-    LANGUAGE plpgsql STABLE
-    AS
-    $BODY$
-    DECLARE
-        r      mytable%ROWTYPE;
-        _round integer;
-    BEGIN
-        -- init vars
-        name    := $1;
-        result2 := 1;       -- abuse result2 as temp var for convenience
+        RETURN NEXT;
 
-    FOR r IN
-        SELECT *
-        FROM   mytable m
-        WHERE  m.name = name
-        ORDER  BY m.round
-    LOOP
-        IF r.round <> _round THEN   -- save result1 before 2nd round
-            result1 := result2;
-            result2 := 1;
-        END IF;
+        END;
+        $BODY$;`;
+        const actual = identify(sql, { dialect: 'psql' });
+        const expected = [
+          {
+            start: 0,
+            end: 902,
+            text: sql,
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
 
-        result2 := result2 * (1 - r.val/100);
-        _round  := r.round;
-    END LOOP;
+      it('should identify postgres "CREATE FUNCTION" statement with case', function () {
+        const sql = `CREATE OR REPLACE FUNCTION af_calculate_range(tt TEXT, tc INTEGER)
+        RETURNS INTEGER IMMUTABLE AS $$
+        BEGIN
+            RETURN CASE tt WHEN 'day' THEN tc * 60 * 60
+                          WHEN 'hour' THEN tc * 60
+                  END;
+        END;
+        $$
+        LANGUAGE PLPGSQL;`;
+        const actual = identify(sql, { dialect: 'psql' });
+        const expected = [
+          {
+            start: 0,
+            end: 299,
+            text: sql,
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
 
-    RETURN NEXT;
+        expect(actual).to.eql(expected);
+      });
 
-    END;
-    $BODY$;`;
-      const actual = identify(sql, { dialect: 'psql' });
-      const expected = [
-        {
-          start: 0,
-          end: 782,
-          text: sql,
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
+      it('should identify mysql "CREATE FUNCTION" statement', function () {
+        const actual = identify("CREATE FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');");
+        const expected = [
+          {
+            start: 0,
+            end: 96,
+            text: "CREATE FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');",
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
 
-    it('should identify postgres "CREATE FUNCTION" statement with case', function () {
-      const sql = `CREATE OR REPLACE FUNCTION af_calculate_range(tt TEXT, tc INTEGER)
-      RETURNS INTEGER IMMUTABLE AS $$
-      BEGIN
-          RETURN CASE tt WHEN 'day' THEN tc * 60 * 60
-                         WHEN 'hour' THEN tc * 60
-                 END;
-      END;
-      $$
-      LANGUAGE PLPGSQL;`;
-      const actual = identify(sql, { dialect: 'psql' });
-      const expected = [
-        {
-          start: 0,
-          end: 285,
-          text: sql,
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
+      it('should identify mysql "CREATE FUNCTION" statement with definer', function () {
+        const actual = identify("CREATE DEFINER = 'admin'@'localhost' FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');", { dialect: 'mysql' });
+        const expected = [
+          {
+            start: 0,
+            end: 126,
+            text: "CREATE DEFINER = 'admin'@'localhost' FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');",
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
 
-      expect(actual).to.eql(expected);
-    });
-
-    it('should identify mysql "CREATE FUNCTION" statement', function () {
-      const actual = identify("CREATE FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');");
-      const expected = [
-        {
-          start: 0,
-          end: 96,
-          text: "CREATE FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');",
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
-
-    it('should identify mysql "CREATE FUNCTION" statement with definer', function () {
-      const actual = identify("CREATE DEFINER = 'admin'@'localhost' FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');", { dialect: 'mysql' });
-      const expected = [
-        {
-          start: 0,
-          end: 126,
-          text: "CREATE DEFINER = 'admin'@'localhost' FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT('Hello, ',s,'!');",
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
-
-    it('should identify sql server "CREATE FUNCTION" statement', function () {
-      const query = `CREATE FUNCTION dbo.ISOweek (@DATE datetime)
-      RETURNS int
-      WITH EXECUTE AS CALLER
-      AS
-      BEGIN
-          DECLARE @ISOweek int;
-          SET @ISOweek= DATEPART(wk,@DATE)+1
-              -DATEPART(wk,CAST(DATEPART(yy,@DATE) as CHAR(4))+'0104');
-      --Special cases: Jan 1-3 may belong to the previous year
-          IF (@ISOweek=0)
-              SET @ISOweek=dbo.ISOweek(CAST(DATEPART(yy,@DATE)-1
-                  AS CHAR(4))+'12'+ CAST(24+DATEPART(DAY,@DATE) AS CHAR(2)))+1;
-      --Special case: Dec 29-31 may belong to the next year
-          IF ((DATEPART(mm,@DATE)=12) AND
-              ((DATEPART(dd,@DATE)-DATEPART(dw,@DATE))>= 28))
-          SET @ISOweek=1;
-          RETURN(@ISOweek);
-      END;`;
-      const actual = identify(query, { dialect: 'mssql' });
-      const expected = [
-        {
-          start: 0,
-          end: 723,
-          text: query,
-          type: 'CREATE_FUNCTION',
-          executionType: 'MODIFICATION',
-        },
-      ];
-      expect(actual).to.eql(expected);
-    });
-
-    it('should identify "CREATE DATABASE" statement', function () {
-      const actual = identify('CREATE DATABASE Profile;');
-      const expected = [
-        {
-          start: 0,
-          end: 23,
-          text: 'CREATE DATABASE Profile;',
-          type: 'CREATE_DATABASE',
-          executionType: 'MODIFICATION',
-        },
-      ];
-
-      expect(actual).to.eql(expected);
+      it('should identify sql server "CREATE FUNCTION" statement', function () {
+        const query = `CREATE FUNCTION dbo.ISOweek (@DATE datetime)
+        RETURNS int
+        WITH EXECUTE AS CALLER
+        AS
+        BEGIN
+            DECLARE @ISOweek int;
+            SET @ISOweek= DATEPART(wk,@DATE)+1
+                -DATEPART(wk,CAST(DATEPART(yy,@DATE) as CHAR(4))+'0104');
+        --Special cases: Jan 1-3 may belong to the previous year
+            IF (@ISOweek=0)
+                SET @ISOweek=dbo.ISOweek(CAST(DATEPART(yy,@DATE)-1
+                    AS CHAR(4))+'12'+ CAST(24+DATEPART(DAY,@DATE) AS CHAR(2)))+1;
+        --Special case: Dec 29-31 may belong to the next year
+            IF ((DATEPART(mm,@DATE)=12) AND
+                ((DATEPART(dd,@DATE)-DATEPART(dw,@DATE))>= 28))
+            SET @ISOweek=1;
+            RETURN(@ISOweek);
+        END;`;
+        const actual = identify(query, { dialect: 'mssql' });
+        const expected = [
+          {
+            start: 0,
+            end: 757,
+            text: query,
+            type: 'CREATE_FUNCTION',
+            executionType: 'MODIFICATION',
+          },
+        ];
+        expect(actual).to.eql(expected);
+      });
     });
 
     describe('should identify "CREATE INDEX" statements', () => {
