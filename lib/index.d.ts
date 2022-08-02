a030c8e38778b04880f0d42836badfafc1a184d8
Nicolas Garnil <nicolas@popsql.com>
2022-08-02 17:38:19 +0200

    remove lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ExecutionType, IdentifyOptions, IdentifyResult } from './defines';
-export type { ExecutionType, Dialect, IdentifyOptions as Options, IdentifyResult as Result, StatementType, } from './defines';
-/**
- * Identifier
- */
-export declare function identify(query: string, options?: IdentifyOptions): IdentifyResult[];
-export declare function getExecutionType(command: string): ExecutionType;
--- lib/tokenizer.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Tokenizer
- */
-import type { Token, State, Dialect } from './defines';
-export declare function scanToken(state: State, dialect?: Dialect): Token;
