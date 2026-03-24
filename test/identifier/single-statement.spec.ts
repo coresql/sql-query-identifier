2f88353211b31e43d69d32dfe9c58202d1ae75a8
Matthew Rathbone <matthew.rathbone@gmail.com>
2026-03-24 15:08:23 -0500

    fix: lint formatting

--- test/identifier/single-statement.spec.ts
+++ test/identifier/single-statement.spec.ts
@@ -1568,29 +1568,26 @@ describe('identifier', () => {
     });
 
     it('should not detect parameters inside MySQL hash comments', () => {
-      const actual = identify(
-        'SELECT * FROM t WHERE x > 1 # is this right? maybe?',
-        { dialect: 'mysql' },
-      );
+      const actual = identify('SELECT * FROM t WHERE x > 1 # is this right? maybe?', {
+        dialect: 'mysql',
+      });
       expect(actual).to.have.length(1);
       expect(actual[0].type).to.eql('SELECT');
       expect(actual[0].parameters).to.eql([]);
     });
 
     it('should still detect real parameters outside MySQL hash comments', () => {
-      const actual = identify(
-        'SELECT * FROM t WHERE x = ? # a comment with ?',
-        { dialect: 'mysql' },
-      );
+      const actual = identify('SELECT * FROM t WHERE x = ? # a comment with ?', {
+        dialect: 'mysql',
+      });
       expect(actual).to.have.length(1);
       expect(actual[0].parameters).to.eql(['?']);
     });
 
     it('should not treat # as comment for non-MySQL dialects', () => {
-      const actual = identify(
-        'SELECT * FROM t WHERE x = ? # not a comment?',
-        { dialect: 'generic' },
-      );
+      const actual = identify('SELECT * FROM t WHERE x = ? # not a comment?', {
+        dialect: 'generic',
+      });
       // generic does not use hash comments, so both ? are detected as parameters
       expect(actual[0].parameters).to.eql(['?', '?']);
     });
